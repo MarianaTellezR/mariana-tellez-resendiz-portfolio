85d4edd1d4b53cbe545cb94b9188450f87f7c99d
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-06-12 10:24:33 -0600

    Created JS that changes heading of career

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+var headings = [
+  "Computer Systems Engineer",
+  "Software Developer",
+  "Front End Developer",
+];
+var i = 0;
+var intervalId = setInterval(function () {
+  document.getElementById("career-heading").innerHTML = headings[i];
+  if (i == headings.length - 1) {
+    i = 0;
+    cleanInterval(intervalId);
+  } else {
+    i++;
+  }
+}, 3000);
